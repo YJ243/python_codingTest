4200a28acdb640f2a3e8d518828e7d17a7dfe365
hyj0225 <60027712+hyj0225@users.noreply.github.com>
2023-09-09 18:01:37 +0900

    20230909

--- chap5_DFS_BFS/5-10_0909.py
+++ chap5_DFS_BFS/5-10_0909.py
@@ -1,59 +1,47 @@
 '''
 date: 2023.09.09
 name: Yejin
-title: 음료수
+title: 음료수_연결요소 찾기(connected component)
 '''
 
+# n, m을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
-graph=[]
 
+# 2차원 리스트의 맵 정보 입력받기
+graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
-
-# 이동할 방향 정의 (상,하,좌,우)
-dx = [1,-1,0,0]
-dy = [0,0,-1,1]
-
-visited = [[False for _ in range(m)] for _ in range(n)]
 
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 def dfs(x,y):
     # 주어진 범위를 벗어나는 경우 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y >= m:
+    if x < 0 or x >= n or y < 0 or y >= m:
         return False
-    # 구멍이 뚫려있는 경우에
-    #
+    
+    # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
-        # 방문했다 표시
-        visited[x][y] = True
-    
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
         
-        if nx < 0 or nx >=n or ny < 0 or ny >= m:
-            continue
-        if graph[nx][ny]:
-            continue
-        if visited[nx][ny]:
-            continue
-        if not visited[nx][ny]:
-            print("dfs 탐색 중", nx, ny)
-            dfs(nx,ny)
-            print("dfs 탐색 끝", nx, ny)
-            return True
-        return True
-    
-    
+        # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
+        # return 값을 사용하지 않기 때문에
+        # 단순히 연결된 모든 노드에 대해서 방문 처리를 수행하기 위한 목적
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        print("function: x, y ", x, y)
+        return True # 현재 위치에 대해서 처음 DFS가 수행이 된 것
+    return False
 
+
+# 모든 노드(위치)에 대하여 음료수 채우기
 result = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i,j):
-            print(i,j)
-            print()
+        # 현재 위치에서 DFS 수행
+        if dfs(i,j): # 처음 방문하는 것
+            print("DFS, i, j: ", i, j)
             result += 1
             
-print(result)
-
-# (1,2)인 경우
+print(result) # 정답 출력
--- chap5_DFS_BFS/5-4.py
+++ chap5_DFS_BFS/5-4.py
@@ -1,7 +1,12 @@
 '''
-date: 2023.09.04
+date: 2023.09.04/2023.09.09
 name: Yejin
 title: recursive condition
+'''
+
+'''
+- 점화식: 재귀함수에서 어떤 함수를 호출해야 하는지 힌트를 줌
+- 초깃값: 종료조건에 대한 정보 줌
 '''
 def recursive_function(i):
     # 100번째 출력했을 때 종료되도록 조건 명시
